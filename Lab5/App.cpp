71cfeb17b17b1e6b916ad81a9d8136340a307e97
Adrian Darian <adarian@ucmerced.edu>
2019-04-10 06:33:42 +0000

    background and cat

--- Lab5/App.cpp
+++ Lab5/App.cpp
@@ -10,18 +10,20 @@ void timer(int id){
     // If you want to manipulate the app in here
     // do it through the singleton pointer
     
+    singleton->background->setX(singleton->background->getX() + 0.01);
+    singleton->redraw();
     glutTimerFunc(16, timer, id);
 }
 
 
 App::App(int argc, char** argv, int width, int height, const char* title): GlutApp(argc, argv, width, height, title){
-   
-
+   background = new TexRect("./bg.png", -2, 1, 4, 2);
+   bobcat = new AnimatedRect("./cat.png", 4, 2, 150, true, true, -0.04, -0.25, 0.8, 0.5);
 }
 
 void App::draw() {
-    TexRect("./bg.png", 2, 2, 0.5, 0.5);
-
+    background->draw(0.0);
+    bobcat->draw(1.0);
 }
 
 void App::keyDown(unsigned char key, float x, float y){
